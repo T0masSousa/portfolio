fa9863a622463e1ef607deae2aa093275e401533
T0mas Sousa <tomasrafael@ua.pt>
2024-04-28 18:53:27 +0100

    Update font-italic class in ProjectsCards.js

--- src/Components/ProjectsCards.js
+++ src/Components/ProjectsCards.js
@@ -64,7 +64,7 @@ const ProjectsCards = () => {
 
                             <h5 className="card-title mb-3 colorTitle">{project.title}</h5>
 
-                            <p className="card-text fw-italic">
+                            <p className="card-text font-italic">
 
                                 {project.languages.map((language, index) => (
 
