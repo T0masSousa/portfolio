6365779379ba25cefae62499813b3af7b45e4c6e
T0mas Sousa <tomasrafael@ua.pt>
2024-04-29 11:10:58 +0100

    Habemos Portfólio 3.0

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import {Outlet, Link} from "react-router-dom";
 
 //BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//CSS
+import '../index.css';
 
 //LOGO
 import {ReactComponent as Logo} from "../Imgs/LOGO.svg";
 
 //FUNÇÃO QUE EXPORTA
 const Layout = () => {
+
+    //UNDERLINE DO SELECIONADO
+    //QUANDO O LINK É SELECIONA, PASSA A TRUE E COLOCA A LINHA EM BAIXO
+    const [selectedLink, setSelectedLink] = useState('/');
 
     return(
 
@@ -18,10 +26,10 @@ const Layout = () => {
                         <Logo className="text-start ms-3 pt-3 pb-4" width="20%" heigth="20%"/>
                     </Link>
                     <div className="ms-auto d-flex pe-5">
-                        <Link to="/About" className="text-decoration-none text-white">
+                        <Link to="/About" className={`text-decoration-none text-white link-hover ${selectedLink === '/About' ? 'active' : ''}`} onClick={() => setSelectedLink('/About')}>
                             <p className="text-end mx-2">About</p>
                         </Link>
-                        <Link to="/Projects" className="text-decoration-none text-white">
+                        <Link to="/Projects" className={`text-decoration-none text-white link-hover ${selectedLink === '/Projects' ? 'active' : ''}`} onClick={() => setSelectedLink('/Projects')}>
                             <p className="text-end mx-2">Projects</p>
                         </Link>
                     </div>
