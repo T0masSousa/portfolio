6bafd53add0de6955309f695babc7fe6c12892c5
T0mas Sousa <tomasrafael@ua.pt>
2024-04-29 13:25:18 +0100

    Update particles configuration and import in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { tsParticles } from "tsparticles";
+import { tsParticles } from "@tsparticles/react";
 
 
 //CSS File
